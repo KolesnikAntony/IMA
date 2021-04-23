b19f43a52e14e7820afce0cc54964a7f94f044f1
Roman <romanshuryhins2rent@gmail.com>
2021-04-23 14:12:42 +0300

    proxy fix v1

---
+++ server/middleware/uploadImg.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+module.exports = async (req, res, next) => {
+	try {
+		if (!req.files || Object.keys(req.files).length === null)
+			return res.status(400).json({message: 'Нет файлов для загрузки'});
+
+		const file = req.files.file;
+
+		console.log(file);
+		if (file.size > 1024 * 1024 * 5) {
+			removeTmp(file.tempFilePath)
+			return res.status(400).json({message: 'Файл слишком большой'});
+		}
+
+		if (file.mimeType !== 'image/jpeg' && file.mimeType !== 'image/png') {
+			removeTmp(file.tempFilePath)
+			return res.status(400).json({message: 'Некорректный формат изображения'});
+		}
+
+		next();
+	} catch (err) {
+		return res.status(500).json({message: err.message});
+	}
+};
+
+const removeTmp = (path) => {
+	fs.unlink(path, err =>{
+		if (err) throw err;
+	})
+};
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const uploadImg = require('../middleware/uploadImg');
+const uploadController = require('../controllers/uploadController');
 
-router.post('/upload');
+
+router.post('/upload', uploadImg, uploadController.uploadAvatar);
 
 module.exports = router;
