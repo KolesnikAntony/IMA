2a9a7bbc52bb8f17817bdb27d8111ad32c66b169
AntonKolesik <kolesiaaa@gmail.com>
2021-05-27 12:47:10 +0300

    shop preloader

--- client/src/componetns/shop/shop.tsx
+++ client/src/componetns/shop/shop.tsx
@@ -8,29 +8,28 @@ import ProductCard from "../product-card/product-card";
 import {getProducts, ProductsInitialStateType} from "../../redux/products-reducer";
 import {useMediaQuery} from "react-responsive";
 import Paginator from '../../common/paginator/pagintaor';
+import {FILTER_TYPES} from "../../constants/constants";
+import Preloader from "../../common/preloader/preloader";
 
 const Shop = () => {
     console.log('render shop');
     const dispatch = useDispatch();
     const products = useSelector<AppStateType, Array<ProductType>>(state => state.products.products);
-    const {totalPages, currentPage, portionSize, sale, itsNew, top , sort} = useSelector<AppStateType, ProductsInitialStateType>(state => state.products);
+    const {totalPages, currentPage, portionSize, selectType , sort, isFetching} = useSelector<AppStateType, ProductsInitialStateType>(state => state.products);
     const isDesktop = useMediaQuery({minWidth: 943});
 
     useEffect(()=> {
         console.log('render shop from useEffect');
-        dispatch(getProducts(currentPage, false, false, false, false));
+        dispatch(getProducts(currentPage, FILTER_TYPES.SELECT_TYPE.ALL, FILTER_TYPES.SORT_TYPE.MAX));
         window.scrollTo(0, 0);
     }, []);
 
     const showCurrentProducts = (currentPage: number) => {
-        dispatch(getProducts(currentPage, sale, itsNew, top , sort));
+        dispatch(getProducts(currentPage, selectType, sort));
         window.scrollTo(0, 0);
     };
 
     //todo make custom Hook for function below
-
-    const [orderOfPrice, setOrderOfPrice] = useState('max');
-    const [productTypeList, setProductTypeList] = useState('all');
 
     const setFilter = (data: FormFilterDataType) => {
         console.log(data);
@@ -38,26 +37,12 @@ const Shop = () => {
 
     const handleOrderPrice = (e: React.FormEvent<HTMLSelectElement>) => {
         let value = e.currentTarget.value;
-        setOrderOfPrice(value);
-        if(value === 'min'){
-            dispatch(getProducts(currentPage, top, itsNew, sale, true));
-        }else if(value === 'max') {
-            dispatch(getProducts(currentPage, top, itsNew, sale,  false));
-        }
+        dispatch(getProducts(currentPage, selectType, value ));
     };
 
     const handleProductList = (e: React.FormEvent<HTMLSelectElement>) => {
         let value = e.currentTarget.value;
-        setProductTypeList(value);
-        if( value === 'all') {
-            dispatch(getProducts(currentPage, false, false, false , sort));
-        }else if(value === 'new'){
-            dispatch(getProducts(currentPage, false, true, false , sort));
-        }else if(value === 'top'){
-            dispatch(getProducts(currentPage, true, false, false , sort));
-        }else if(value === 'sale'){
-            dispatch(getProducts(currentPage, false, false, true , sort));
-        }
+        dispatch(getProducts(currentPage, value, sort));
     };
 
     const [showMobFilter, setShowMobFilter] = useState(false);
@@ -77,23 +62,26 @@ const Shop = () => {
                             {isDesktop || <button className="shop__select" onClick={handleShowMobFilter}>
                                 Filter
                             </button>}
-                            <select className="shop__select" value={orderOfPrice} onChange={(e) => handleOrderPrice(e)}>
-                                <option value="min" className="shop__select-item">min</option>
-                                <option value="max" className="shop__select-item">max</option>
+                            <select className="shop__select" value={sort} onChange={(e) => handleOrderPrice(e)}>
+                                <option value={FILTER_TYPES.SORT_TYPE.MIN} className="shop__select-item">min</option>
+                                <option value={FILTER_TYPES.SORT_TYPE.MAX} className="shop__select-item">max</option>
                             </select>
-                            <select className="shop__select" value={productTypeList}
+                            <select className="shop__select" value={selectType}
                                     onChange={(e) => handleProductList(e)}>
-                                <option value="all" className="shop__select-item">all</option>
-                                <option value="new" className="shop__select-item">new</option>
-                                <option value="top" className="shop__select-item">top</option>
-                                <option value="sale" className="shop__select-item">sale</option>
+                                <option value={FILTER_TYPES.SELECT_TYPE.ALL} className="shop__select-item">all</option>
+                                <option value={FILTER_TYPES.SELECT_TYPE.NEW} className="shop__select-item">new</option>
+                                <option value={FILTER_TYPES.SELECT_TYPE.TOP} className="shop__select-item">top</option>
+                                <option value={FILTER_TYPES.SELECT_TYPE.SALE} className="shop__select-item">sale</option>
                             </select>
                             {showMobFilter && !isDesktop && <Filter products={products} setFilter={setFilter}/>}
                         </div>
-                        <div className="shop__products">
-                            {products.map(el => <ProductCard key={el.id} {...el}/>)}
-                        </div>
-                        <Paginator currentPage={currentPage} totalPages={totalPages}  portionSize={portionSize} showCurrentProducts={showCurrentProducts}/>
+                        {isFetching ? <Preloader type='products-list'/> :
+                        <>
+                            <div className="shop__products">
+                                {products.length !== 0 ? products.map(el => <ProductCard key={el.id} {...el}/>): <span className='shop__empty'>List is empty...</span> }
+                            </div>
+                            <Paginator currentPage={currentPage} totalPages={totalPages}  portionSize={portionSize} showCurrentProducts={showCurrentProducts}/>
+                        </>}
                     </div>
                 </div>
             </div>
