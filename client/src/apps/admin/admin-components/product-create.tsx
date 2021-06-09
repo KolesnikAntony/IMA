7597b0e6e4917405176f21f95524a3e5e20e26cb
AntonKolesik <kolesiaaa@gmail.com>
2021-06-09 16:16:29 +0300

    admin auth

--- client/src/apps/admin/admin-components/product-create.tsx
+++ client/src/apps/admin/admin-components/product-create.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Create, SimpleForm, TextInput} from 'react-admin';
+import {Create, SimpleForm, TextInput, ImageInput} from 'react-admin';
 
 const ProductCreate = (props: any) => {
     return <Create title='Create a product' {...props}>
-        <SimpleForm>
+        <SimpleForm enctype="multipart/form-data">
             <TextInput source='title'/>
             <TextInput multiline source='description'/>
             <TextInput source='price'/>
+            <ImageInput source='imageSrc'/>
         </SimpleForm>
     </Create>
 }
